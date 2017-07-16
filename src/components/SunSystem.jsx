5f74b203394b07285f8da4b264421b712c08926f
Denis <denis_777@ukr.net>
2017-07-16 18:34:51 +0300

    Final2

---
+++ src/components/SunSystem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, RouteHandler, Link} from 'react-router';
+
+
+
+
+
+const SunSystem = React.createClass({
+
+    render() {
+        
+        return (
+
+        <img alt="SunSystem" src='img/sun.jpg' className="sun"/>
+        );
+    }
+});
+
+export default SunSystem;
