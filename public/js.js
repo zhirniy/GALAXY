bb701327b606657d6a8f454e87202c1d568301f7
zhirniy <denis_777@ukr.net>
2017-07-29 16:01:41 +0300

    Update js.js

--- public/js.js
+++ public/js.js
@@ -15,6 +15,8 @@ window.onload = function () {
       var a11 = document.querySelectorAll('div[data-reactid=".0.1.0"]')[0];
 
       var arr  =[a0,a1,a2,a3,a4,a5,a6,a7,a8,a9];
+       a10.style.backgroundColor = "black";
+       a11.style.backgroundImage = "url('body.jpg')";
      
 	
      
